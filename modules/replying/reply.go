18cd7acb5e3b2cf699eb601a0e805584e09d4ec3
miyasereina <1448472422@qq.com>
2022-05-10 22:20:45 +0800

    fix：once

--- modules/replying/reply.go
+++ modules/replying/reply.go
@@ -92,12 +92,11 @@ func re(str string) []string {
 	return st
 }
 
-func groupReply(msg *message.GroupMessage, c *client.QQClient) {
+var once sync.Once
 
-	o := &sync.Once{}
-	o.Do(func() {
-		Morning(c)
-	})
+func groupReply(msg *message.GroupMessage, c *client.QQClient) {
+	Morning(c)
+	fmt.Println(S)
 	reply := []string{}
 	ver := ""
 	if re(msg.ToString()) != nil {
@@ -206,22 +205,28 @@ func groupReply(msg *message.GroupMessage, c *client.QQClient) {
 
 }
 
+var S int
+
 func Morning(c *client.QQClient) {
 	cr := cron.New()
-	// 把需要定时执行的函数都丢里面
-	// 设置时间分别是秒，分，时，日，月, 星期
-	cr.AddFunc("0 0 8 * * ? ", func() {
-		s := []string{
-			"妈妈早上好，最喜欢妈妈了妈妈今天也加油哦，快去叫爸爸起床！",
-			"nana想妈妈了，没有妈妈，nana好可怜，nana最喜欢妈妈了",
-			"橘蓼：蓉宝蓉宝蓉宝最喜欢蓉宝了，蓉宝今天也是元气慢慢的一天哦",
-			"橘蓼：蓉宝今天上班吧，如果不上班就来摸橘宝嘛，考试辛苦哭哭",
-			"おはようお母さんま,なな母いなくで寂しい,ハグほしい",
-			"橘蓼：蓉宝蓉宝蓉宝蓉宝起床了，太阳晒屁股了，该吃饭了哦",
-			"橘蓼：姐姐早上了哦，亲亲サワサワ、姉さんますごい可愛い，大好きだよ",
-		}
-		m := message.NewSendingMessage().Append(message.NewText(s[rand.Intn(len(s))]))
-		c.SendGroupMessage(735214237, m)
+	once.Do(func() {
+		S++
+		cr.AddFunc("0 52 8 * * ? ", func() {
+			s := []string{
+				"妈妈早上好，最喜欢妈妈了妈妈今天也加油哦，快去叫爸爸起床！",
+				"nana想妈妈了，没有妈妈，nana好可怜，nana最喜欢妈妈了",
+				"橘蓼：蓉宝蓉宝蓉宝最喜欢蓉宝了，蓉宝今天也是元气满满的一天哦",
+				"橘蓼：蓉宝今天上班吧，如果不上班就来摸橘宝嘛，考试辛苦哭哭",
+				"おはようお母さんま,なな母いなくで寂しい,ハグほしい",
+				"橘蓼：蓉宝蓉宝蓉宝蓉宝起床了，太阳晒屁股了，该吃饭了哦",
+				"橘蓼：姐姐早上了哦，亲亲サワサワ、姉さんますごい可愛い，大好きだよ",
+			} // 把需要定时执行的函数都丢里面
+			//设置时间分别是秒，分，时，日，月, 星期
+			m := message.NewSendingMessage().Append(message.NewText("520！！")).Append(message.NewText(s[rand.Intn(len(s))]))
+			c.SendGroupMessage(735214237, m)
+
+		})
+
 	})
 	cr.Start()
 
